89f79c0da0d2fda6fb0fb21b66fac6e4b13cd036
lucas <lucsalmeida004@gmail.com>
2025-09-26 20:20:27 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string frasedislalia,  flase;
+
+Console.WriteLine("digite uma flase");
+ flase = Console.ReadLine();
+
+frasedislalia = flase
+ 
+ .Replace("r","l")
+  .Replace("R","L");
+ 
+Console.WriteLine(frasedislalia);
+
+
+
